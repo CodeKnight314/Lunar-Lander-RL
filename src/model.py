dbbadc4681ea69174f8663c8ba40a0d369d2f3ee
CodeKnight314 <tang.ri@northeastern.edu>
2025-04-03 17:17:51 +0100

    Minor bugfix on DQN model parameters

--- src/model.py
+++ src/model.py
@@ -12,11 +12,11 @@ class DiscreteQN(nn.Module):
     def __init__(self, input_dim: int, output_dim: int):
         super(DiscreteQN, self).__init__()
         self.net = nn.Sequential(*[
-            nn.Linear(input_dim, 256),
+            nn.Linear(input_dim, 128),
             nn.ReLU(),
-            nn.Linear(256, 256),  
+            nn.Linear(128, 128),  
             nn.ReLU(),
-            nn.Linear(256, output_dim)
+            nn.Linear(128, output_dim)
         ])
         
     def forward(self, x):
