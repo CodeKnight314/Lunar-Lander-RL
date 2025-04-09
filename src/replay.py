d5570f0b6643bea3b53b6b7c488a1a0cd49b3e51
CodeKnight314 <tang.ri@northeastern.edu>
2025-04-10 00:36:07 +0100

    Bugfixes

--- src/replay.py
+++ src/replay.py
@@ -21,6 +21,8 @@ class ReplayBuffer:
 
         if isinstance(actions[0], (int, np.integer)):
             actions = torch.tensor(np.array(actions), dtype=torch.int64).unsqueeze(1)
+        else: 
+            actions = torch.stack(actions).to(torch.float32)
 
         rewards = torch.tensor(np.array(rewards), dtype=torch.float32).unsqueeze(1)
         next_states = torch.tensor(np.array(next_states), dtype=torch.float32)
@@ -28,7 +30,7 @@ class ReplayBuffer:
 
         if self.device == "cuda":
            states = states.to(self.device)
-           actions = states.to(self.device)
+           actions = actions.to(self.device)
            rewards = rewards.to(self.device)
            next_states = next_states.to(self.device)
            dones = dones.to(self.device)
